09ba22e92608180a1ec7d7de0420cdf762fa8063
Suwitchaya Bunyarattaphun <6110613103@cn103>
2021-05-02 20:23:18 +0700

    Visitor Pattern Example

---
+++ Visitor_pattern/src/visitor_pattern/Book.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visitor_pattern;
+
+/**
+ *
+ * @author noeyyeon
+ */
+public class Book implements ItemElement{
+	private int price;
+	private String isbnNumber;
+	
+	public Book(int cost, String isbn){
+		this.price=cost;
+		this.isbnNumber=isbn;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+
+	public String getIsbnNumber() {
+		return isbnNumber;
+	}
+        
+	
+	public int accept(ShoppingCartVisitor visitor) {
+		return visitor.visit(this);
+	}
+
+}
---
+++ Visitor_pattern/src/visitor_pattern/Fruit.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visitor_pattern;
+
+/**
+ *
+ * @author noeyyeon
+ */
+public class Fruit implements ItemElement {
+	
+	private int pricePerKg;
+	private int weight;
+	private String name;
+	
+	public Fruit(int priceKg, int wt, String nm){
+		this.pricePerKg=priceKg;
+		this.weight=wt;
+		this.name = nm;
+	}
+	
+	public int getPricePerKg() {
+		return pricePerKg;
+	}
+
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+	
+	public int accept(ShoppingCartVisitor visitor) {
+		return visitor.visit(this);
+	}
+
+}
